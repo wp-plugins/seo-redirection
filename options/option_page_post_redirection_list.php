4c488408480c662a86c157c3d83564f06dce5f0a
fakhris <fakhris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 17:18:56 +0000

    hot fix
    git-svn-id: https://plugins.svn.wordpress.org/seo-redirection/trunk@1100543 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options/option_page_post_redirection_list.php
+++ options/option_page_post_redirection_list.php
@@ -28,7 +28,7 @@ $util->info_option_msg("You have a cache plugin installed <b>'" . $util->there_i
 function go_search(){
 var sword = document.getElementById('search').value;
 	if(sword!=''){
-		window.location = "<?=$rlink?>&search=" + sword ;
+		window.location = "<?php echo $rlink?>&search=" + sword ;
 	}else
 	{
 		alert('Please input any search words!');
@@ -43,9 +43,9 @@ var sword = document.getElementById('search').value;
 <table border="0" width="100%">
 	<tr>
 		<td align="left">
-		<input onkeyup="if (event.keyCode == 13) go_search();" style="height: 30px;" id="search" type="text" name="search" value="<?=$util->get('search')?>" size="40">
+		<input onkeyup="if (event.keyCode == 13) go_search();" style="height: 30px;" id="search" type="text" name="search" value="<?php echo $util->get('search')?>" size="40">
 		<a onclick="go_search()" href="#"><div class="search_link">Search</div></a> 
-		<a href="<?=$util->get_current_parameters('search')?>"><div class="see_link">Show All</div></a>
+		<a href="<?php echo $util->get_current_parameters('search')?>"><div class="see_link">Show All</div></a>
 		</td>
 	</tr>
 </table>
