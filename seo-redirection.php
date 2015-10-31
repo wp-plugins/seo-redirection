d0081b3d3f741d5fb9956989bc1c3a8ab37a94fb
fakhris <fakhris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-31 18:03:10 +0000

    hotfix
    git-svn-id: https://plugins.svn.wordpress.org/seo-redirection/trunk@1277010 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seo-redirection.php
+++ seo-redirection.php
@@ -519,7 +519,7 @@ global $util;
         
         if(is_multisite())
         {
-            echo '<p align="center"><b><font size="5"><font color="#FF0000">As in the plugin description from the first version, this version (free) does 
+            echo '<p align="center"><b><font size="5"><font color="#FF0000">As in the plugin description since the first version, this version (free) does 
 not support Multisite WordPress installation, if you have a redirects that alleady added, please contact us at http://www.clogica.com/contact-us to fix the issue for you, or see the Premium Version at the 
 following link</font>:</font></b></p>
 <p align="center"><b>
